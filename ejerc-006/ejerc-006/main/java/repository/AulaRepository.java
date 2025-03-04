513d6b0ae950ec9647175df7de63ae06bb1134d9
Jagofer <pejgofer@gmail.com>
2025-03-04 10:37:25 +0100

    Delete ejerc-006/ejerc-006 directory

--- ejerc-006/ejerc-006/main/java/repository/AulaRepository.java
+++
@@ -1,9 +0,0 @@
-package es.santander.ascender.ejerc_006.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.santander.ascender.ejerc_006.Model.Aula;
-
-public interface AulaRepository extends JpaRepository< Aula, Long> {
-
-}
--- ejerc-006/ejerc-006/main/java/service/CRUDOperation.java
+++
@@ -1,5 +0,0 @@
-package es.santander.ascender.ejerc_006.Service;
-
-public enum CRUDOperation {
-    CREATE, READ, UPDATE, DELETE;
-}
